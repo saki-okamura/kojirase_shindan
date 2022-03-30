47a354f5dcef72deb8155184e7aa9970b6bf92de
saki-okamura <ykhmsk@gmail.com>
2022-03-30 16:06:44 +0900

    ツイートを表示確認ビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/help' => 'static_pages#help'
   get '/privacy' => 'static_pages#privacy'
   get '/terms' => 'static_pages#terms'
+  get '/tweet', to: 'twitter_analyses#twitter_analysis'
 end
