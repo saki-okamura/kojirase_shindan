d26a8c2507695e5b71b5b0cd9d614ce0efb980ce
saki-okamura <ykhmsk@gmail.com>
2022-03-31 21:45:49 +0900

    twitterの再導入’ wq :wq exit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def twitter_client
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ''
-      config.consumer_secret     = ''
-      config.access_token        = ''
-      config.access_token_secret = ''
-    end
-  end
 end
