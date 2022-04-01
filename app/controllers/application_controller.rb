9fc968a0446b98624478f78087941a718dda254e
saki-okamura <ykhmsk@gmail.com>
2022-04-01 18:04:49 +0900

    application_controller.rbの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def twitter_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.access_token = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_SECRET']
+    end
+  end
+
   # 環境変数の設定
   Dotenv.load
   HOSTNAME = ENV['HOSTNAME']
