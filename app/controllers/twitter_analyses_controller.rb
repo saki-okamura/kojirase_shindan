963b4277189347687b57df6a766d0c38f99f7455
saki-okamura <ykhmsk@gmail.com>
2022-04-01 18:07:15 +0900

    twitter_analyses_controller.rbの修正

--- app/controllers/twitter_analyses_controller.rb
+++ app/controllers/twitter_analyses_controller.rb
@@ -1,16 +1,9 @@
 class TwitterAnalysesController < ApplicationController
+  before_action :twitter_client
 
   def twitter_analysis
-    client = Twitter::REST::Client.new do |config|
-      # 取得したキーを入れる
-      config.consumer_key = ENV['CONSUMER_KEY']
-      config.consumer_secret = ENV['CONSUMER_SECRET']
-      config.access_token = ENV['ACCESS_TOKEN']
-      config.access_token_secret = ENV['ACCESS_SECRET']
-    end
-
     @tweets = []
-    client.user_timeline("rouninsaikouw", exclude_replies: false, include_rts: true).take(25).each do |tw|
+    @client.user_timeline("rouninsaikouw", exclude_replies: false, include_rts: true).take(25).each do |tw|
       @tweets << tw.text
     end
   end
