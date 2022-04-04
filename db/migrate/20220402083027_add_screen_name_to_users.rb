0ea4e8b6d2480ac39272b15cddda4462aa591a26
saki-okamura <ykhmsk@gmail.com>
2022-04-04 17:29:06 +0900

    データベースの削除

--- db/migrate/20220402083027_add_screen_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddScreenNameToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :screen_name, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_083027) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.text "screen_name"
-  end
 
 end
