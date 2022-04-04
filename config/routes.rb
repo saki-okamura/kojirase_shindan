289eac5904f1fa2aa7adefb162a0e0c3403e66b9
saki-okamura <ykhmsk@gmail.com>
2022-04-04 16:50:17 +0900

    404エラーのview追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/privacy', to: 'static_pages#privacy'
   get '/terms', to: 'static_pages#terms'
+
+  get '*path', to: 'application#render_404'
 end
