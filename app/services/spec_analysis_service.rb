9b1b7545f09085715294109eda24bd0b5c44de19
saki-okamura <ykhmsk@gmail.com>
2022-04-06 17:00:05 +0900

    spec_analysis_service.rbの導入

---
+++ app/services/spec_analysis_service.rb
@@ -0,0 +1,38 @@
+class SpecAnalysisService
+  # 異性のスペック度の診断
+
+  def initialize(user, twitter_client, tweets)
+    @user = user
+    @twitter_client = twitter_client
+    @tweets = tweets
+  end
+
+  def call
+    words = ['高い', '安い', '円', '筋トレ', 'ダイエット', '身長', '体重', '年収', '万円', '学歴', '職業']
+    spec = 0
+    words.each do |word|
+      spec += @tweets.filter{|tweet| tweet.text.include?(word)}.size
+    end
+
+    spec_point = case spec
+              when 0..5
+                1
+              when 6..20
+                1.5
+              when 21..30
+                2
+              when 21..30
+                2.5
+              when 31..40
+                3
+              when 41..50
+                3.5
+              when 51..60
+                4
+              when 61..70
+                4.5
+              else
+                5
+              end
+  end
+end
