b6527c72620c1cec21b09648a4e4980eee5099e1
saki-okamura <ykhmsk@gmail.com>
2022-04-08 22:05:09 +0900

    トップ画面とフッターの編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get '/results' => 'results#show'
 
   root to: 'static_pages#top'
-  get '/help', to: 'static_pages#help'
   get '/privacy', to: 'static_pages#privacy'
   get '/terms', to: 'static_pages#terms'
 
