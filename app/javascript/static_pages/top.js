6436df364fc65567bf8a64173ca2b88d44abe346
saki-okamura <ykhmsk@gmail.com>
2022-04-09 23:07:34 +0900

    診断フォーム周りのview修正’ ’

---
+++ app/javascript/static_pages/top.js
@@ -0,0 +1,6 @@
+function clickAnalysisButton() {
+  const analysis_form = document.getElementById("analysis-button");
+  const analysis_button = document.getElementById("analysis-button-wrapper");
+  analysis_form.style.display = "block";
+  analysis_button.style.display = "none";
+}
