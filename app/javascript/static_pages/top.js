53f5e349a3f77eb2317c6fe9602bbf0618f8fa98
saki-okamura <ykhmsk@gmail.com>
2022-04-15 17:10:25 +0900

    結果表示のページの修正

--- app/javascript/static_pages/top.js
+++
@@ -1,6 +0,0 @@
-function clickAnalysisButton() {
-  const analysis_form = document.getElementById("analysis-button");
-  const analysis_button = document.getElementById("analysis-button-wrapper");
-  analysis_form.style.display = "block";
-  analysis_button.style.display = "none";
-}
