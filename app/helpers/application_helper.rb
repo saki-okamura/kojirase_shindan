28d92df8db12b78418f581b32bee59e2105bb52b
saki-okamura <ykhmsk@gmail.com>
2022-04-21 21:39:31 +0900

    OGPの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,30 @@ module ApplicationHelper
     base_title = ' -こじらせ診断- '
     title.empty? ? base_title : title + base_title
   end
+
+  def default_meta_tags
+    {
+      site: 'こじらせ診断',
+      title: 'そこのあなた、こじらせてない？',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ツイートからこじらせ度を診断するアプリ',
+      keywords: 'こじらせ, メンヘラ, 婚活, 女性, 診断',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('top-girl.png') },
+        { href: image_url('top-girl.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' }
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP'
+      }
+    }
+  end
 end
