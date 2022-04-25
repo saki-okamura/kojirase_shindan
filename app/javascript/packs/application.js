205cb34416c32b4b60ab186bed1e2b90ee950ed3
saki-okamura <ykhmsk@gmail.com>
2022-04-25 14:24:58 +0900

    リロードしたらトップに行くように記述

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,10 @@ ActiveStorage.start()
 require('jquery')
 require('show.js')
 
+$(function () {
+  $('html,body').animate({ scrollTop: 0 }, '1');
+});
+
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
